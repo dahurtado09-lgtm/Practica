447c10ed3b50103d8428feb9fc2593bb27672b2f
Alejandro <da.hurtado09@ciaf.edu.co>
2025-09-28 16:02:11 -0500

    Ejercicios de practica Js

---
+++ ejercicio-1.js
@@ -0,0 +1,15 @@
+//Declara dos números y muestra en consola la suma, resta, multiplicación y división
+const num1 = 90
+const num2 = 6
+console.log(`La suma de ${num1} y ${num2} es: ${num1 + num2}`)
+console.log(`\nLa resta de ${num1} y ${num2} es: ${num1 - num2}`)
+console.log(`\nLa multiplicacion de ${num1} y ${num2} es: ${num1 * num2}`)
+console.log(`\nLa division de ${num1} y ${num2} es: ${num1 / num2}\n`)
+
+//Crea un array con 5 nombres y recórrelo con un for para mostrarlos en consola.
+let nombres = [
+    "Diego", "Alejandro", "Jhon", "jean", "Elias"
+];
+for (let i = 0; i < nombres.length ; i++  )
+    {console.log(nombres[i])}
+//Escribe una función que reciba un número y devuelva la tabla de multiplicar de ese número.
